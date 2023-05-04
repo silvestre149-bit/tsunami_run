575cdfad50cd9403836c40943ac6515e4eed5b36
Silvestre149-bit <contato.matheus.silvestre@gmail.com>
2023-05-04 19:27:17 -0300

    sprite

--- platform.py
+++ platform.py
@@ -27,11 +27,11 @@ class Player:
         self.x = x
         self.y = y
         self.width = 50
-        self.height = 50
         self.vel_x = 0
         self.vel_y = 0
         self.is_jumping = False
-        self.image = pygame.image.load("/home/silvestre/Área de trabalho/Faculdade/Jogos/tsunami/plataformas/3 Objects/3 Power lines/4.png").convert_alpha()
+        self.image = pygame.image.load("/home/silvestre/Área de trabalho/Faculdade/Jogos/tsunami/tsunami_run/sprite_0.png").convert_alpha()
+        self.height = self.image.get_height()
         self.rect = self.image.get_rect(x=self.x, y=self.y)
 
     def move(self, keys_pressed, platforms):
@@ -57,7 +57,7 @@ class Player:
 
         if self.y >= WINDOW_HEIGHT - self.height:
             self.is_jumping = False
-            self.y = WINDOW_HEIGHT - self.height
+            self.y = WINDOW_HEIGHT - self.height - 1
             self.vel_y = 0
 
         # Verifica a colisão do jogador com as plataformas
@@ -67,6 +67,33 @@ class Player:
                     self.rect.bottom = platform.rect.top
                     self.vel_y = 0
                     self.is_jumping = False
+                            
+ 
+        # Verifica a colisão do jogador com o chão
+        # if self.rect.colliderect(ground.rect):
+        #     if self.vel_y > 0 and self.rect.bottom <= ground.rect.top + self.height/2:
+        #         self.is_jumping = False
+        #         self.rect.bottom = ground.rect.top
+        #         self.vel_y = 0
+        #     else:
+        #         self.rect.top = ground.rect.bottom
+        #         self.is_jumping = True
+        #         self.vel_y = 0
+
+        # # Verifica a colisão do jogador com as plataformas
+        #     for platform in platforms + [ground]:
+        #         if self.rect.colliderect(platform.rect):
+        #             if self.vel_y > 0 and self.rect.bottom <= platform.rect.top + self.height/2:
+        #                 self.is_jumping = False
+        #                 self.rect.bottom = platform.rect.top
+        #                 self.vel_y = 0
+        #             else:
+        #                 self.rect.top = platform.rect.bottom
+        #                 self.is_jumping = True
+        #                 self.vel_y = 0
+        #             break
+
+
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
@@ -81,6 +108,18 @@ class Platform:
 
     def draw(self, surface):
         pygame.draw.rect(surface, (0, 0, 255), self.rect)
+
+class Ground(Platform):
+    def __init__(self):
+        super().__init__(0, WINDOW_HEIGHT - 10)  # Chama o construtor da classe Platform
+        self.image = pygame.image.load("/home/silvestre/Área de trabalho/Faculdade/Jogos/tsunami/plataformas/1 Tiles/Tile_37.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (WINDOW_WIDTH, self.image.get_height()))
+        self.rect = self.image.get_rect(x=self.x, y=WINDOW_HEIGHT - self.height)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)  # Desenha o sprite do chão na tela
+
+ground = Ground()
 
 # Gera as plataformas aleatoriamente
 platforms = []
@@ -103,6 +142,9 @@ while True:
     player.move(keys_pressed, platforms)
 
     window.fill(BACKGROUND_COLOR)
+
+    #Desenha o chão
+    ground.draw(window)
 
     # Desenha as plataformas na tela
     for platform in platforms:
